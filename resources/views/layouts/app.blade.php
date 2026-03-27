801deeda49347ed6e80bf123cc07784e9fd206e7
Guadalupeja <lupita_ja_93@hotmail.com>
2026-03-26 21:48:17 -0600

    Arreglar error en favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
-<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-<link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
-
   <title>@yield('title', config('app.name', 'Laravel'))</title>
   @hasSection('meta_description')
     <meta name="description" content="@yield('meta_description')">
