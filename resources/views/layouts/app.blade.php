138d97be60225809bde1b5c126765cadcfe4126f
Guadalupeja <lupita_ja_93@hotmail.com>
2026-03-31 12:11:57 -0600

    Agregar favicon sitio y blog

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,13 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+<link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
+
   <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
   <title>@yield('title', config('app.name', 'Laravel'))</title>
   @hasSection('meta_description')
